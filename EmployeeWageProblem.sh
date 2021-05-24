81b0e9abcb0377a205eb1e6dc762de9fcc6c1920
shubham314-wizard <skhairnar314@gmail.com>
2021-05-24 18:38:05 +0530

    EmployeeWageProblem file is added

--- EmployeeWageProblem.sh
+++ EmployeeWageProblem.sh
@@ -1,5 +1,15 @@
+
 #! bin/bash -x
 
 #EmployeeWageProblem
 
-echo "welcome to shell script of empwage"
+echo  "welcome to shell script of empwage"
+
+isEmployeePresent=$((RANDOM%2))
+
+if [ $isEmployeePresent -eq 1 ]
+then
+ 	echo "Employee is present"
+else
+	echo "Employee is present"
+fi
